e138f851d73236df071aee1e372bc7b8935ab670
jpswt <joel_swt@yahoo.com>
2022-11-21 22:44:21 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [points, setPoints] = useState([]);
+	const [poppedPoints, setPoppedPoints] = useState([]);
+
+	const handleClick = (e) => {
+		const { clientX, clientY } = e;
+		setPoints([
+			...points,
+			{
+				x: clientX,
+				y: clientY,
+			},
+		]);
+	};
+
+	const handleUndo = () => {
+		const newPoints = [...points];
+		const popped = newPoints.pop();
+		if (!poppedPoints) return;
+		setPoppedPoints([...poppedPoints, popped]);
+		setPoints(newPoints);
+	};
+
+	const handleRedo = () => {
+		const newPoints = [...points];
+		const newPopped = [...poppedPoints];
+		const popped = newPopped.pop();
+		if (!popped) return;
+		newPoints.push(popped);
+		setPoints([...points, popped]);
+		setPoppedPoints(newPopped);
+	};
+
+	return (
+		<>
+			<button disabled={points.length === 0} onClick={handleUndo}>
+				Undo
+			</button>
+			<button disabled={poppedPoints.length === 0} onClick={handleRedo}>
+				Redo
+			</button>
+			<div className="App" onClick={handleClick}>
+				{points.map((point, idx) => (
+					<div
+						className="point"
+						style={{ left: point.x - 10, top: point.y - 10 }}
+						key={idx}
+					></div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default App;
